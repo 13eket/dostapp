71d0392c148c9c095bacbd4743d504ace5cce8f8
Beket Bayemirov <bayemirov@gmail.com>
2025-02-23 01:37:58 +0100

    init

---
+++ src/main/java/com/bayemirov/dostapp/DostappApplication.java
@@ -0,0 +1,18 @@
+package com.bayemirov.dostapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DostappApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(DostappApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DostappApplication.class, args);
+		log.info("Something changed! Yoy yoys ");
+	}
+
+}
