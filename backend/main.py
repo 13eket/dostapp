36b30c43fcbc7e28f9088d475f3ff9b97fdd7357
Beket Bayemirov <bayemirov@gmail.com>
2025-06-03 19:18:36 +0200

    change ports

--- backend/main.py
+++ backend/main.py
@@ -149,4 +149,4 @@ async def init_payment(request: Request):
 # For local testing
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
